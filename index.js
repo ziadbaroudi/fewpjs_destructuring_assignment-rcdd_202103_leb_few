77a49e032ffcfb9d34bb39801d072ed334aecc9d
Ziad Baroudi <ziad_baroudi@hotmail.com>
2021-04-17 23:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const [blackAndWhite, , black, pink ] = farmAnimals.split(" ");
 // Arrays
 
 // 4. Use destructuring to assign appropriate variables using the color names.
-
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
 // 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
